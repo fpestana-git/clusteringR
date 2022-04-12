5415f70dba830afa59a7f37cbbfd110788bea7ed
fpestana-git <72916953+fpestana-git@users.noreply.github.com>
2022-04-12 14:34:52 +0200

    update function

--- R/clusteringSCT.R
+++ R/clusteringSCT.R
@@ -1,8 +1,16 @@
-# Function that takes a dataset (either spatial or scRNA-seq), creates Seurat object, runs SCT normalization, PCA, etc, clustering
+#' Seurat clustering with SCT transformation
+#' 
+#' # Function that takes a dataset (either spatial or scRNA-seq), creates Seurat object, runs SCT normalization, PCA, etc, clustering
+#' 
+#' @param datasetObject dataset object
+#' @param metadataObject metadata object
+#' @param selectFeatures choose if features are to be selected (default NULL)
+#' @param selectedFeatureNames list of features to use on the clustering
+#' @param pcaValueOptimal define a specific PCA value to use (default NULL). Otherwise it will calculate an optimal PCA value.
+#' @param datasetName define the name of the dataset
+#' @param resolutionValue define the resolution value to use for clustering (default 0.2)
 
-clusteringSCT <- function(datasetObject,metadataObject,selectFeatures = NULL,selectedFeatureNames,pcaValueOptimal = NULL, datasetName, labelValue = T,resolutionValue = 0.2){
-  # Spatial data
-  
+clusteringSCT <- function(datasetObject,metadataObject,selectFeatures = NULL,selectedFeatureNames,pcaValueOptimal = NULL, datasetName,resolutionValue = 0.2){
   # Select only non pcdh genes for running the clustering of the data
   if (selectFeatures) {
     #selectFeatures <- grep(pattern = "^Pcdh|Il33",x = rownames(datasetObject),invert = T,value = T)
