f1bc0f670013373280552f364be291fab9e65987
fpestana-git <72916953+fpestana-git@users.noreply.github.com>
2022-04-21 13:02:27 +0200

    Small update

--- R/createSpatialSeurat.R
+++ R/createSpatialSeurat.R
@@ -1,10 +1,20 @@
-# Function that creates a spatial object based on sample Counts and sample Coordinates
-createSpatialSeurat <- function(sampleCounts, sampleCoordinates){
-  sampleCoordinates <- sampleCoordinates
-  slide.seq <- CreateSeuratObject(counts = sampleCounts, assay="Spatial")
+#' Seurat spatial object
+#' 
+#' Function that takes a spatial dataset and respective x and y coordinates to generate the spatial map of an individual sample
+#' 
+#' @param datasetObject dataset object
+#' @param metadataObject metadata object that includes X and Y coordinates for each cell
+#'
+#' @export createSpatialSeurat
+
+
+# This function creates a spatial object based on sample Counts and sample Coordinates
+createSpatialSeurat <- function(datasetObject, metadataObject){
+  #sampleCoordinates <- sampleCoordinates
+  slide.seq <- CreateSeuratObject(counts = datasetObject, assay="Spatial")
   
-  coord.df <- data.frame(x=sampleCoordinates$X, y=sampleCoordinates$Y, stringsAsFactors=FALSE) # (stringsAsFactors only if also have a separate barcodes column)
-  rownames(coord.df) <- rownames(sampleCoordinates)
+  coord.df <- data.frame(x=metadataObject$X, y=metadataObject$Y, stringsAsFactors=FALSE) # (stringsAsFactors only if also have a separate barcodes column)
+  rownames(coord.df) <- rownames(metadataObject)
   
   slide.seq@images$image <-  new(
     Class = 'SlideSeq',
@@ -13,7 +23,7 @@ createSpatialSeurat <- function(sampleCounts, sampleCoordinates){
     coordinates = coord.df
   )
   # Update spatial metadata
-  slide.seq@meta.data <- cbind(slide.seq@meta.data,sampleCoordinates)
+  slide.seq@meta.data <- cbind(slide.seq@meta.data,metadataObject)
   
   slide.seq
 }
